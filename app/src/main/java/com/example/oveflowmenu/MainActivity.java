19fb4760c253ceea33cee69e26722ed6b76d5ca0
= <=>
2019-05-29 09:59:46 +0700

    menu

---
+++ app/src/main/java/com/example/oveflowmenu/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.oveflowmenu;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        ConstraintLayout mainLayout =
+                (ConstraintLayout) findViewById(R.id.layoutView);
+        switch (item.getItemId()) {
+            case R.id.menu_red:
+                // lệnh thực thi khi chọn RED
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                mainLayout.setBackgroundColor(android.graphics.Color.RED);
+                return true;
+            case R.id.menu_green:
+                // lệnh thực thi khi chọn GREEN
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                mainLayout.setBackgroundColor(android.graphics.Color.GREEN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
